d3c70e8dfb548642cf2f9a2797e4a4198b01051d
Roy Reshef <64038871+royreshef279@users.noreply.github.com>
2024-03-30 22:07:54 -0400

    Update accumulator_tb.v

--- accumulator_tb.v
+++ accumulator_tb.v
@@ -64,12 +64,14 @@ initial begin
     #5 rst = 0; A=1;
     forever #10 A = A + 1;
 end
-
+// Clock cycle is 10ns
 always #5 clk = ~clk;
 
 initial begin
     #10; 
     repeat (4) begin
+        //Adjust the time (change #90 to another time) to add more inputs to be accumulated 
+        //In this case, (90+10)/10 gives the number of inputs to be added
         #90 rst = 1;  
         #10 rst = 0;  
     end
