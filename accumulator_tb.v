68c8194563b2ffbccf41310a4e887e69d7518d2c
Roy Reshef <64038871+royreshef279@users.noreply.github.com>
2024-03-30 22:09:17 -0400

    Update accumulator_tb.v

--- accumulator_tb.v
+++ accumulator_tb.v
@@ -11,32 +11,6 @@ accumulator dut(
     .rst(rst),
     .sum(sum)
 );
-//initial begin
-//rst =0; clk =0; A = 0;
-//#5
-//rst=1;
-//#5
-//rst=0;A=1;
-//#100
-//rst =1;A=0;
-//#10
-//rst=0;
-//#90
-//rst=1;
-//#10
-//rst=0;
-//#90
-//rst=1;
-//#10
-//rst=0;
-//#90
-//rst=1;
-//#5
-//$stop;
-//end
-//always #5 clk = ~clk;
-//always #10 A = A+1;
-
 initial begin
     clk=0;
     rst=0;
