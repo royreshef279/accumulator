d9f9464b81cfcfdcebc4575fe9e0c70d21dc5716
Roy Reshef <64038871+royreshef279@users.noreply.github.com>
2024-03-30 22:08:17 -0400

    Update accumulator_tb.v

--- accumulator_tb.v
+++ accumulator_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/28/2024 12:26:28 PM
-// Design Name: 
-// Module Name: accumulator_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module accumulator_tb();
 reg [31:0]A;
